ee238c17d679be27f8ea30affd623f9dbeb626e2
Nikko-Kostenko <70935960+Nikko-Kostenko@users.noreply.github.com>
2020-09-29 19:45:03 +0300

    Add files via upload

---
+++ Home tasks/Home task 3/Task_3_Converter/Task_3_Converter/Converter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Task_3_Converter
+{
+    class Converter
+    {
+        private double UAH_to_USD;
+        private double UAH_to_EUR;
+
+        public Converter(double USD, double EUR)
+        {
+            UAH_to_EUR = EUR;
+            UAH_to_USD = USD;
+        }
+
+        public double ConvertUSD(double UAH)
+        {
+            double USD;
+
+            try
+            {
+                if (UAH_to_USD == 0.0)
+                {
+                    throw new Exception("деление на 0");
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+
+            finally
+            {
+                USD = UAH / UAH_to_USD;
+            }
+
+            return USD;
+        }
+
+        public double ConvertEUR(double UAH)
+        {
+            double EUR;
+
+            try
+            {
+                if (UAH_to_EUR == 0)
+                {
+                    throw new Exception("деление на 0");
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+
+            finally
+            {
+                EUR = UAH / UAH_to_EUR;
+            }
+
+            return EUR;
+        }
+    }
+}
---
+++ Home tasks/Home task 3/Task_3_Converter/Task_3_Converter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_3_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter Translate = new Converter(25,30);
+            Console.WriteLine(Translate.ConvertEUR(100));
+        }
+    }
+}
